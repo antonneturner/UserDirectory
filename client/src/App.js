6b97914355f857c9f368d500cef86032681e0f24
winfrom within <antonneturner@gmail.com>
2021-03-25 16:08:28 -0400

    create components folder, create table.js, import table, import api, create api.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Table from "./components/Table";
+import Api from "./utils/API";
 function App() {
+  const [users, setusers] = useState([]);
+  useEffect(() => {
+    // use the API to return user data and set to users state
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table />
     </div>
   );
 }
